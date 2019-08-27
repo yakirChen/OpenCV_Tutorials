f29ac573f1c1f1af2252c4dd5cc5350a66d5bd83
yakirChen <iamyakirchen@outlook.com>
2019-08-27 15:08:23 +0800

    OpenCV 4.0.0 -> 4.1.1

--- OpenCV_Tutorials/main.cpp
+++ OpenCV_Tutorials/main.cpp
@@ -13,22 +13,22 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        printf("使用: OpenCV_Tutorials <Image_Path>\n");
         return -1;
     }
+
     Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
+    image = imread(argv[1], 1);
+    if (!image.data) {
+        printf("没有照片数据 \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    namedWindow("显示照片", WINDOW_AUTOSIZE);
+    imshow("显示照片", image);
     waitKey(0);
     return 0;
 }
